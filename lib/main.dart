ea238ee891ae03688f6e2ba9e444d1ef41e9cfa4
katarinasokcevic <katarina.sokcevic.vk@gmail.com>
2023-06-10 00:30:50 +0200

    Camera preview

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'FaceTally',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[100],
+      appBar: AppBar(title: const Text("FaceTally")),
+      body: SafeArea(
+        child: Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                await availableCameras().then((value) => Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => CameraPage(cameras: value))));
+              },
+              child: const Text("Open the camera"),
+            )),
+      ),
+    );
+  }
+}
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({Key? key, required this.cameras}) : super(key: key);
+
+  final List<CameraDescription>? cameras;
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _cameraController;
+  bool _isRearCameraSelected = true;
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera(widget.cameras![0]);
+  }
+
+  Future initCamera(CameraDescription cameraDescription) async {
+    _cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+    try {
+      await _cameraController.initialize().then((_) {
+        if (!mounted) return;
+        setState(() {});
+      });
+    } on CameraException catch (e) {
+      debugPrint("camera error $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red[100],
+        body: SafeArea(
+          child: Stack(children: [
+            (_cameraController.value.isInitialized)
+                ? CameraPreview(_cameraController)
+                : Container(
+                color: Colors.red[100],
+                child: const Center(child: CircularProgressIndicator())),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+                      color: Colors.pinkAccent),
+                  child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    Expanded(
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          iconSize: 30,
+                          icon: Icon(
+                              _isRearCameraSelected
+                                  ? CupertinoIcons.switch_camera
+                                  : CupertinoIcons.switch_camera_solid,
+                              color: Colors.white),
+                          onPressed: () {
+                            setState(
+                                    () => _isRearCameraSelected = !_isRearCameraSelected);
+                            initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
+                          },
+                        )),
+                  ]),
+                )),
+          ]),
+        ));
+  }
+}
+
